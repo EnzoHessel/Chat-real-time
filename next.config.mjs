3591dd4c29979d63e8cc2f224fb6718b3f9d50e2
Enzo Hessel <enzo.carmo@salg.com.br>
2024-05-19 20:54:03 -0400

    updtade next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
-    domains: ['utfs.io'],
     remotePatterns: [
       {
         protocol: "https",
@@ -32,4 +25,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default nextConfig
